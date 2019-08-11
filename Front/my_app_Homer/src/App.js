bd8933147004fd8a3662cf86452241daa59aaf97
Eva Martin <evamediacion@hotmail.com>
2019-08-11 19:14:56 +0200

    020-Creando un formulario

--- Front/my_app_Homer/src/App.js
+++ Front/my_app_Homer/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SignUp from './Components/SignUp'
-import { lstat } from 'fs';
+/*import { lstat } from 'fs';*/
 
 function App() {
   return (
     <div className="App">
        <img src={logo} className="App-logo" alt="logo" />
+       <form>
         <SignUp/>
+        </form>
     </div>
   );
 }
--- Front/my_app_Homer/src/Components/SignUp.js
+++ Front/my_app_Homer/src/Components/SignUp.js
@@ -5,32 +5,103 @@ export default class SignUp extends Component {
         super(props);
 
         this.state = {
-            email: ""
-        };
-        
-        this.updateEmailField = this.updateEmailField.bind(this);
-      }
-
-        updateEmailField(e) {
-          /*const{value, email} = e.target;*/
-          this.setState({
-            value: e.target.value
-          })
+            email: "",
+            password:"",
+            verificationp:"",
+            firstname:"",
+            lastname:"",
         }
+
+        this.onChange = this.onChange.bind(this);
+        this.submitForm = this.submitForm.bind(this);
+      };
     
+      
+
+        onChange(e) {
+          const{value, name} = e.target;
+          this.setState({
+            [name]: value
+          })
+          console.log(this.state);
+        }
+
+        submitForm(e){
+          e.preventDefault();
+        }
+      
+    
+
   render() {
     return(
     <div>
       <h1>
         {this.state.value}
       </h1>
-      <input 
-      type='email' 
-      name = 'email' 
-      onChange = {this.updateEmailField} 
-      />
-       
-    </div>
+    
+    <div className = "FormHommer">
+      
+    <form  onSubmit={this.submitForm}>
+      <fieldset>
+       <div className="form-data" >
+         <label htmlFor="email">email</label>
+        <input
+          type="email"
+          id="email"
+          name = "email"
+          onChange = {this.onChange}
+          value= {this.state.email} 
+          />
+      </div>
+      <div className="form-data">
+        <label htmlFor>Password</label>
+        <input
+        type= "password"
+        id= "password"
+        name= "password"
+        onChange = {this.onChange}
+        value= {this.state.password}
+        />
+      </div>
+      <div className = "form-data">
+        <label htmlFor>Verification password</label>
+        <input
+        type= "password"
+        id="verificationp"
+        name= "verificationp"
+        onChange = {this.onChange}
+        value = {this.state.verificationp}
+        />
+      </div>
+      <div className= "form-data">
+        <label htmlFor>First Name</label>
+        <input
+        type= "text"
+        id="firstname"
+        name= "firstname"
+        onChange = {this.onChange}
+        value = {this.state.firstname}
+        />
+      </div>
+      <div className= "form-data">
+        <label htmlFor>Last Name</label>
+        <input
+        type="text"
+        id="lastname"
+        name="lastname"
+        onChange= {this.onChange}
+        value= {this.state.lastname}
+        />
+      </div>
+
+      <div className ="form-data">
+        <input type="submit" value="Send" />
+      </div>
+
+    </fieldset>
+  </form>
+</div> 
+</div>
     );
   }
 }
